5ac0f83d64256fd72ef004a2e059ede1f4eba7ab
Max Fathauer <maxfathauer@gmail.com>
2022-10-20 19:46:54 -0400

    setting up buttons

---
+++ assets/js/cal.js
@@ -0,0 +1,6 @@
+const disReturn = document.getElementById("display");
+
+const btnrow1 = document.getElementById('btns1');
+const btnrow2 = document.getElementById('btns2');
+const btnrow3 = document.getElementById('btns3');
+
