dc843ae131bdfe6299b08ad5b62e805fd9659a4f
Max Fathauer <maxfathauer@gmail.com>
2022-10-22 15:13:23 -0400

    forgot to push yesterday

--- assets/js/cal.js
+++ assets/js/cal.js
@@ -1,6 +1,2 @@
 const disReturn = document.getElementById("display");
 
-const btnrow1 = document.getElementById('btns1');
-const btnrow2 = document.getElementById('btns2');
-const btnrow3 = document.getElementById('btns3');
-
