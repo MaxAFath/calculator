3d4536f991117c06b8c5e2ba2ac0ca4619f9278a
Max Fathauer <maxfathauer@gmail.com>
2022-12-05 12:35:54 -0500

    added functions +-*/, need to add calculate functionality

--- assets/js/cal.js
+++ assets/js/cal.js
@@ -1,9 +1,61 @@
 var disReturn = document.getElementById("display");
+var value1 = null;
+var value2 = null;
+var total;
 
 const calc = document.getElementById("calculator");
 const buttons = document.getElementsByClassName("btn");
+const perts = document.getElementsByClassName("function");
+
+onload.preventDefault();
 
 
 buttons.onclick = function (value) {
-    disReturn += value;
+    if (disReturn.value != null) {
+        ((disReturn.value * 10) + value);
+    }
+    else {
+        disReturn.value = value;
+    }
+
+    disReturn.innerHTML += value;
+
 }
+
+perts.onclick = function (value) {
+    disReturn.innerHTML = value;
+    if (value1 == null) {
+        value1 = disReturn.value;
+        disReturn.value = 0;
+    }
+    else if ((value1 != null) & (value2 == null)) {
+        value2 = dis.disReturn.value;
+        disReturn.value = 0;
+    }
+    else {
+        switch (value) {
+            case '+':
+                total = value1 + value2;
+                break;
+            case '-':
+                total = value1 - value2;
+                break;
+            case '*':
+                total = value1 * value2;
+                break;
+            case '/':
+                total = value1 / value2;
+                break;
+            default:
+                console.log('error');
+        }
+    }
+
+}
+
+calc.onclick = function () {
+    disReturn.innerHTML = total
+}
+
+
+
