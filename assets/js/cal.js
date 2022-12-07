78f9787faf4621f3be908dd6542cfdd07648ee4a
Max Fathauer <maxfathauer@gmail.com>
2022-12-06 21:30:20 -0500

    I think I finished the calculator. Will test it tomorrow or thursday

--- assets/js/cal.js
+++ assets/js/cal.js
@@ -1,6 +1,5 @@
 var disReturn = document.getElementById("display");
 var value1 = null;
-var value2 = null;
 var total;
 
 const calc = document.getElementById("calculator");
@@ -11,14 +10,12 @@ onload.preventDefault();
 
 
 buttons.onclick = function (value) {
-    if (disReturn.value != null) {
-        ((disReturn.value * 10) + value);
+    if ((disReturn.value != null) || (disReturn.value = 0)) {
+        disReturn.value += ((disReturn.value * 10) + value);
     }
     else {
         disReturn.value = value;
     }
-
-    disReturn.innerHTML += value;
 
 }
 
@@ -27,24 +24,30 @@ perts.onclick = function (value) {
     if (value1 == null) {
         value1 = disReturn.value;
         disReturn.value = 0;
+        disReturn.innerHTML = value;
     }
-    else if ((value1 != null) & (value2 == null)) {
-        value2 = dis.disReturn.value;
-        disReturn.value = 0;
-    }
+
     else {
         switch (value) {
             case '+':
-                total = value1 + value2;
+                total += value1;
+                disReturn.value = 0;
+                disReturn.innerHTML = value;
                 break;
             case '-':
-                total = value1 - value2;
+                total -= value1;
+                disReturn.value = 0;
+                disReturn.innerHTML = value;
                 break;
             case '*':
-                total = value1 * value2;
+                total *= value1;
+                disReturn.value = 0;
+                disReturn.innerHTML = value;
                 break;
             case '/':
-                total = value1 / value2;
+                total /= value1;
+                disReturn.value = 0;
+                disReturn.innerHTML = value;
                 break;
             default:
                 console.log('error');
@@ -54,7 +57,31 @@ perts.onclick = function (value) {
 }
 
 calc.onclick = function () {
-    disReturn.innerHTML = total
+    let value = disReturn.innerHTML.charAt(0);
+    switch (value) {
+        case '+':
+            total += value1;
+            disReturn.value = total;
+            disReturn.innerHTML = total;
+            break;
+        case '-':
+            total -= value1;
+            disReturn.value = total;
+            disReturn.innerHTML = total;
+            break;
+        case '*':
+            total *= value1;
+            disReturn.value = total;
+            disReturn.innerHTML = total;
+            break;
+        case '/':
+            total /= value1;
+            disReturn.value = total;
+            disReturn.innerHTML = total;
+            break;
+        default:
+            console.log('error');
+    }
 }
 
 
